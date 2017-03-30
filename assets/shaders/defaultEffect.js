19e5a232abf359cd6d2bfa6064117aba5d762735
ccajas <chris.cajas.m@gmail.com>
2017-03-30 17:54:27 -0500

    New models and revamped shader code

--- assets/shaders/defaultEffect.js
+++ assets/shaders/defaultEffect.js
@@ -12,8 +12,12 @@ DefaultEffect = (function()
 
 	DefaultEffect.prototype =
 	{
+		// You could consider these as shader uniforms
+
 		cam: [0, 0, 1],
 		l: [1, -0.15, 0],
+		ambient: 0.35,
+		spcolor: [0.2, 0.225, 0.375],
 
 		vertex: function(vs_in)
 		{
@@ -22,7 +26,7 @@ DefaultEffect = (function()
 			var normal = vs_in[2];
 			var ratio = this.scr_h / this.scr_w;
 
-			var nx, ny, nz;
+			//var nx, ny, nz;
 
 			// Rotate vertex and normal
 			var mat_w = this.m_world;
@@ -30,18 +34,16 @@ DefaultEffect = (function()
 
 			// Transform vertex to screen space
 
-			var x = m.floor((rt[0] / 2 + 0.5 / ratio) * this.scr_w * ratio); 
-			var y = m.floor((rt[1] / 2 + 0.95) * this.scr_h);
-			var z = m.floor((rt[2] / 2 + 0.5) * 65536);
+			var x = ((rt[0] * 0.5 + 0.5 / ratio) * this.scr_w * ratio)|0; 
+			var y = ((rt[1] * 0.5 + 0.97) * this.scr_h)|0;
+			var z = ((rt[2] * 0.5 + 0.5) * 65536)|0;
 
 			return [[x, y, z], vs_in[1], normal, this.r];
 		},
 
 		fragment: function(ps_in, color)
 		{
-			var ambient = 0.35;
 			var light = [];
-			var spcolor = [0.1, 0.125, 0.175];
 
 			// Sample diffuse and normal textures
 			var t = [235, 235, 235];//this.texture.sample(null, ps_in[0]);
@@ -53,13 +55,14 @@ DefaultEffect = (function()
 			var intensity = m.max(Vec3.dot(nnt, nl), 0);
 
 			// Using Blinn reflection model
-			var view = this.cam;
   			var r = Vec3.reflect(nl, nnt); // reflected light
-  			var spec = m.pow(m.max(Vec3.dot(r, view), 0), 8) * 4;
+  			var spec = m.max(Vec3.dot(r, this.cam), 0);
+  			spec *= spec * spec * 2;
+  			if (spec > 1) spec = 1;
 
-			light[0] = intensity + ambient + spec * spcolor[0];
-			light[1] = intensity + ambient + spec * spcolor[1];
-			light[2] = intensity + ambient + spec * spcolor[2];
+			light[0] = intensity + this.ambient + spec * this.spcolor[0];
+			light[1] = intensity + this.ambient + spec * this.spcolor[1];
+			light[2] = intensity + this.ambient + spec * this.spcolor[2];
 
 			// Output color	
 			color[0] = m.min(t[0] * light[0], 255);
